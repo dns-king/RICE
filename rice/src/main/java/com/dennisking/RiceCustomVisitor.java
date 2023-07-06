4c2383dd6f2b63bf64994d3a1351dc894763a3ea
Dennis King <118165654+dns-king@users.noreply.github.com>
2023-07-06 11:25:01 +0000

    updated!

--- rice/src/main/java/com/dennisking/RiceCustomVisitor.java
+++ rice/src/main/java/com/dennisking/RiceCustomVisitor.java
@@ -1,8 +1,23 @@
 
 package com.dennisking;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RiceCustomVisitor extends RiceBaseVisitor<Object> {
 
+    // Create a symbol table to store variable values
+    private Map<String, Object> symbolTable;
 
+    public CustomVisitor() {
+        symbolTable = new HashMap<>();
+    }
+
+    @Override
+    public Void visitRiceProgram(RiceParser.RiceProgramContext ctx) {
+        // Visit the cookCodeBlock
+        visit(ctx.cookCodeBlock);
+        return null;
+    }
 
 }
