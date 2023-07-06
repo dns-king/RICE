0134c5a0a00f45ccdd6453d47f7e3a98c19e339c
Dennis King <118165654+dns-king@users.noreply.github.com>
2023-07-06 11:16:00 +0000

    updated!

--- rice/src/main/java/com/dennisking/Main.java
+++ rice/src/main/java/com/dennisking/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         RiceCustomVisitor visitor = new RiceCustomVisitor();
-        visitor.executeRiceFile("rice/src/main/java/com/dennisking/Program.rice");
+        visitor.executeRiceFile("rice/src/main/java/com/dennisking/program.rice");
 
     }
 }
