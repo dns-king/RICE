881cd12431383d92090685d7db6529296f04d2f8
Dennis King <118165654+dns-king@users.noreply.github.com>
2023-08-23 09:24:54 +0000

    updated!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 
-int main() {
+int main()
+{
     llvm::LLVMContext context;
     llvm::Module module("my_module", context);
 
     //create LLVM IR for Rice
 
-    module.dump();  // Dump the generated LLVM IR
+    module.dump(); // Dump the generated LLVM IR
 
     return 0;
 }
